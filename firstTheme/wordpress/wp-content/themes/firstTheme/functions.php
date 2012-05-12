b9170e186db5f4952712995cddb240bc855d6e4e
aaron hubbard <aaron@localhost.localdomain>
2012-05-12 14:46:48 -0400

    finished firstTheme

---
+++ firstTheme/wordpress/wp-content/themes/firstTheme/functions.php
@@ -0,0 +1,9 @@
+<?php
+	//set content width for the theme
+	
+		//$content_width = 500;
+	
+	// Theme setup
+	add_action( 'after_setup_theme', 'firstTheme_themesetup');
+	function firstTheme_themesetup(){
+	}
