aa82582725e6bf9f406f98a7cba5bcccd17dc7dd
aaron hubbard <aaron@localhost.localdomain>
2012-05-25 17:57:24 -0400

    git made me

--- firstTheme/wordpress/wp-content/themes/firstTheme/index.php
+++ firstTheme/wordpress/wp-content/themes/firstTheme/index.php
@@ -9,34 +9,27 @@
     ?>
     
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header>
-               
-            </header>
             <?php
                 // The content
                 the_content();
-                
                 // If singular and comments are open
                 if ( is_singular() && comments_open() )
                 comments_template( '', true );
             ?>
         </article>
-        
     <?php 
         // Loop ends
         endwhile; 
         // Nothing in the loop?
         else : 
-    ?>
-    
+    ?>          
         <article id="post-0" class="post no-results not-found">
             <header>
                 <h2 class="entry-title">Nothing Found</h2>
             </header>
             <p>We're sorry, but we couldn't find anything for your. Please try and search for whatever it was you were looking for.</p>
             <?php get_search_form(); ?>
-        </article>
-        
+        </article>        
     <?php 
         // And we're done
         endif; 
